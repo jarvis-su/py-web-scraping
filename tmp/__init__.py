e60a5ccacb64f96d6ea5919b66741ba51a95bf7a
JarvisSu <Jarvis_su@126.com>
2018-01-08 16:04:53 +0800

    no more comments

--- tmp/__init__.py
+++ tmp/__init__.py
@@ -1,4 +1,16 @@
 import requests
 
-r=requests.get('https://www.baidu.com')
-r.status_code
+
+def getHTMLText(url):
+    try :
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return 'Get exception '
+
+if __name__ == '__main__':
+    url = "http://www.baidu.com"
+    url = "www.baidu.com"
+    print(getHTMLText(url))
