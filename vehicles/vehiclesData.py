08e79b0181ea8762bc7c5f66e23a3f9a95d866f7
JarvisSu <Jarvis_su@126.com>
2018-03-01 21:48:06 +0800

    update ignore

--- vehicles/vehiclesData.py
+++ vehicles/vehiclesData.py
@@ -8,3 +8,16 @@ vehicles = pd.read_csv("./data/vehicles.csv")
 print(vehicles.head())
 print(len(vehicles))
 #- 查看有多少观测点（行）和多少变量（列）
+
+
+#描述汽车油耗等数据
+print( len(vehicles.columns))
+print(vehicles.columns)
+
+#查看年份信息
+print( len(pd.unique(vehicles.year)))
+#总共的年份p
+print( min(vehicles.year))
+#最小年份
+print( max(vehicles.year))
+#最大的年份
